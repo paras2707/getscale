cf4a8946b0c6e73bc76f81cd44c878e668f11477
Lavneesh Garg <lavneesh583@Lavneeshs-MacBook-Air.local>
2022-03-13 02:13:18 +0530

    homepage completed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,18 @@
 import classes from "./Card.module.css";
+import { FiThumbsUp } from "react-icons/fi";
+import { FiThumbsDown } from "react-icons/fi";
+import { FaRegCommentAlt } from "react-icons/fa";
 function Card(props) {
   return (
     <div className={classes.card}>
-      {/* {props.text} */}
+      <div className={classes.title}> {props.text} </div>
       <div className={classes.image}></div>
       <div className={classes.side}></div>
       <div className={classes.tittle}></div>
+      <div className={classes.company}>{props.company}</div>
+      <div className={classes.up}><FiThumbsUp /></div>
+      <div className={classes.bottom}><FiThumbsDown /></div>
+      <div className={classes.comment}><FaRegCommentAlt /></div>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,10 @@
 import Card from "../components/Card";
 import NavBar from "../NavBar";
-
+import { IoFilter } from "react-icons/io5";
+import { RiArrowUpDownFill } from "react-icons/ri";
+import { IconContext } from "react-icons";
+import Button from "../components/Button";
+// import { AiOutlinePlusCircle } from "react-icons/ai";
 const HomePage = () => {
   return (
     <div>
@@ -12,12 +16,44 @@ const HomePage = () => {
           width: "100%",
           alignItems: "center",
           marginTop: "80px",
+          
         }}
       >
-        <Card text="Description 1" />
-        <Card text="Description 1" />
-        <Card text="Description 1" />
-        <Card text="Description 1" />
+        <div style={{ display: "flex", flexDirection: "row", marginLeft: "-700px", marginBottom: "5px"}}>
+          <IconContext.Provider value={{ size: "30px", color: "#727272" }}>
+
+            <div> <IoFilter /></div>
+            <div style={{ marginLeft: "28px" }}><RiArrowUpDownFill /></div>
+
+          </IconContext.Provider>
+          <div style={{ 
+            position: "absolute",
+            width: "223px",
+            height: "29px",
+            marginLeft: "277px",
+            marginTop: "3px",
+            fontFamily: "Open Sans",
+            fontStyle: "normal",
+            fontWeight: "600",
+            fontSize: "21px",
+            lineHeight: "29px",
+            textAlign: "center",
+            color: "#727272" }}
+          > Scale Up Your Passion</div>
+        </div>
+            <div > <Button style={{ 
+              position: "absolute",
+              width: "35px",
+              height: "35px",
+              marginLeft: "352px",
+              marginTop: "-35px",
+              background: "#5EA0FF",
+              borderRadius: "50%"
+            }}>+</Button> </div>
+        <Card text="Description " company="Company" />
+        <Card text="Description " company="Company"/>
+        <Card text="Description " company="Company"/>
+        <Card text="Description " company="Company"/>
       </div>
     </div>
   );
